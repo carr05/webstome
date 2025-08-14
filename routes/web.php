48bd2d3c0cf331c301ed5becf88a0a44a8c3f0f8
hestisnm <hestisuryaningrum123@gmail.com>
2025-08-14 09:20:13 +0700

    baru

--- routes/web.php
+++ routes/web.php
@@ -10,23 +10,7 @@ Route::get('/', [adminControler::class, 'formLogin'])->name('login');
 Route::post('/login', [adminControler::class, 'prosesLogin'])->name('login.post');
 Route::get('/home', [adminControler::class, 'home'])->name('home');
 
-
-// Layout
-Route::get('/layout1', [adminControler::class, 'layout'])->name('layout1');
-Route::get('/layout1/index', [adminControler::class, 'index'])->name('layout1.index');
-
-// Berita (CRUD)
-Route::get('/berita', [beritaController::class, 'index'])->name('berita.index');
-Route::get('/berita/create', [beritaController::class, 'create'])->name('berita.create');
-Route::post('/berita', [beritaController::class, 'store'])->name('berita.store');
-Route::get('/berita/{id}/edit', [beritaController::class, 'edit'])->name('berita.edit');
-Route::put('/berita/{id}', [beritaController::class, 'update'])->name('berita.update');
-Route::delete('/berita/{id}', [beritaController::class, 'destroy'])->name('berita.destroy');
-
-// Agenda
-
 Route::get('/layout', [adminControler::class, 'layout'])->name('layout1');
-
 Route::get('/agenda', [adminControler::class, 'agenda'])->name('agenda');
 Route::get('/berita', [adminControler::class, 'berita'])->name('berita');
 Route::get('/galeri', [adminControler::class, 'galeri'])->name('galeri');
